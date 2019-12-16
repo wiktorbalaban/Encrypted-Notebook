759d945061ce8da9d5bc6cde04b8169dab2daf21
Wiktor <wiktorbalaban@gmail.com>
2019-12-16 14:40:19 +0100

    Repair the prefs keys and authorization request

--- app/src/main/java/com/example/encryptednotebook/InitialVector/SharedPreferencesInitialVector.java
+++ app/src/main/java/com/example/encryptednotebook/InitialVector/SharedPreferencesInitialVector.java
@@ -6,6 +6,8 @@ import java.util.Base64;
 
 public class SharedPreferencesInitialVector implements InitialVectorProxy {
 
+    private final static String keyBegin = "IV_";
+
     private SharedPreferences prefs;
 
     public SharedPreferencesInitialVector(SharedPreferences prefs) {
@@ -14,13 +16,13 @@ public class SharedPreferencesInitialVector implements InitialVectorProxy {
 
     @Override
     public byte[] load(String key) {
-        String ivString = prefs.getString(key, null);
+        String ivString = prefs.getString(keyBegin + key, "");
         return Base64.getDecoder().decode(ivString);
     }
 
     @Override
     public void save(byte[] iv, String key) {
         String ivString = Base64.getEncoder().encodeToString(iv);
-        prefs.edit().putString(key, ivString).apply();
+        prefs.edit().putString(keyBegin + key, ivString).apply();
     }
 }
